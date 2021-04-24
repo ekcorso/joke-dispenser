b814d3a326cdb546ab91b0693264108ea78a09c6
Emily Corso <ekcorso@gmail.com>
2021-04-23 18:27:30 -0700

    Create TypeSelectionViewController and stub out functions

---
+++ JokeDispenser/TypeSelectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TypeSelectionViewController.swift
+//  JokeDispenser
+//
+//  Created by Emily Corso on 4/22/21.
+//
+
+import UIKit
+
+class TypeSelectionViewController: UIViewController {
+    
+    
+    override func loadView() {
+        <#code#>
+    }
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
