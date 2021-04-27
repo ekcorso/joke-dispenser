e14154092dd8a8ff312406e2a5eb535169ea9faa
Emily Corso <ekcorso@gmail.com>
2021-04-27 15:52:39 -0700

    WIP: Create buttons for single and two-part jokes. Bug: buttons both lead to a table view of two-part jokes.

--- JokeDispenser/TypeSelectionViewController.swift
+++ JokeDispenser/TypeSelectionViewController.swift
@@ -14,29 +14,50 @@ class TypeSelectionViewController: UIViewController {
     
     override func loadView() {
         super.loadView()
-        //Stuff
+        
+        self.navigationItem.title = "Select Joke Type"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //VC general set-up
         view.backgroundColor = .black
         
         //Set up button for single-part jokes
         singleJokeButton.setTitle("See one-liners", for: .normal)
         view.addSubview(singleJokeButton)
+        singleJokeButton.translatesAutoresizingMaskIntoConstraints = false
         singleJokeButton.backgroundColor = .blue
         singleJokeButton.setTitleColor(.white, for: .normal)
-        singleJokeButton.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
-        singleJokeButton.addTarget(self, action: #selector(didTapSinglePartJokeButton), for: .touchUpInside)
+        //singleJokeButton.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        singleJokeButton.addTarget(self, action: #selector(didTapSingleJokeButton), for: .touchUpInside)
         
         //Set up button for two-part jokes
         twoPartJokeButton.setTitle("See two-part jokes", for: .normal)
-    
+        view.addSubview(twoPartJokeButton)
+        twoPartJokeButton.translatesAutoresizingMaskIntoConstraints = false
+        twoPartJokeButton.backgroundColor = .red
+        twoPartJokeButton.setTitleColor(.white, for: .normal)
+        //twoPartJokeButton.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        twoPartJokeButton.addTarget(self, action: #selector(didTapTwoPartJokeButton), for: .touchUpInside)
+        
+        setConstraints()
     }
     
-    @objc func didTapSinglePartJokeButton() {
-
+    @objc func didTapSingleJokeButton() {
+        //Takes user to the main VC and sets the query type to single part jokes
+        let viewController = ViewController()
+        viewController.jokeAPI.fetchSinglePartJokes()
+        navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+    @objc func didTapTwoPartJokeButton() {
+        //Takes user to the main VC and sets the query type to two part jokes
+        let viewController = ViewController()
+        viewController.jokeAPI.fetchTwoPartJokes()
+        navigationController?.pushViewController(viewController, animated: true)
     }
 }
 extension TypeSelectionViewController {
@@ -45,10 +66,13 @@ extension TypeSelectionViewController {
     
     func setConstraints() {
         NSLayoutConstraint.activate([
-//            tableView.topAnchor.constraint(equalTo: view.topAnchor),
-//            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
-//            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
-//            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            singleJokeButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200),
+            singleJokeButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
+            //singleJokeButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            singleJokeButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 10),
+            twoPartJokeButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
+            twoPartJokeButton.leadingAnchor.constraint(equalTo: singleJokeButton.leadingAnchor),
+            twoPartJokeButton.trailingAnchor.constraint(equalTo: singleJokeButton.trailingAnchor)
         ])
     }
 }
